38f81da9835eeed74c2cc299741a0a4b7e302fda
banseok <banseok9204@naver.com>
2024-06-18 21:51:42 +0900

    first commit

---
+++ src/main/java/me/whiteship/inflearnjavatest/InflearnJavaTestApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.inflearnjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/whiteship/inflearnjavatest/InflearnJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.inflearnjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
