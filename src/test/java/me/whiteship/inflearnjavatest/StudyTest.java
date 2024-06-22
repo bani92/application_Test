e32cf47834fc2ccf1217a54aaa2c5d5dcdc9847f
banseok <banseok9204@naver.com>
2024-06-22 16:06:36 +0900

    JUnit 5 테스트반복 ArgumentsAggregator, SimpleArgumentConverter

--- src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnjavatest/StudyTest.java
@@ -1,7 +1,16 @@
 package me.whiteship.inflearnjavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
@@ -31,9 +40,27 @@ class StudyTest {
 
     @DisplayName("스터디 만들기!")
     @ParameterizedTest(name = "{index} - {displayName} message = {0}")
-    @ValueSource(strings = {"날씨가" ,"많이", "추워지고", "있네요"})
-    void parameterizedTest(String message) {
-        System.out.println(message);
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    // 인자값이 여러개일때 ArgumentsAggregator(인터페이스)
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
+    }
+    // 인자값이 하나일때 SimpleArgumentConverter(클래스)
+    static class StudyConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object source, Class<?> aClass) throws ArgumentConversionException {
+            assertEquals(Study.class, aClass, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
     }
 
     @BeforeAll
