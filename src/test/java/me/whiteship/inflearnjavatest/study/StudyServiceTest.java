1dc61d1b78c8b5a4d33216be8202339e69ea0cae
banseok <banseok9204@naver.com>
2024-06-28 21:49:29 +0900

    Mock 객체 스터빙

--- src/test/java/me/whiteship/inflearnjavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnjavatest/study/StudyServiceTest.java
@@ -19,6 +19,9 @@ import java.util.Optional;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -33,8 +36,37 @@ class StudyServiceTest {
     void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
 
         StudyService studyService = new StudyService(memberService,studyRepository);
-
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("keesun@email.com");
+        when(memberService.findById(any())).thenReturn(Optional.of(member)).thenThrow(new RuntimeException()).thenReturn(Optional.empty());
+
+        Optional<Member> byId = memberService.findById(1L);
+        assertEquals("keesun@email.com",byId.get().getEmail());
+
+
+        assertThrows(RuntimeException.class, () -> {
+            memberService.findById(1L);
+        });
+
+        Optional<Member> byId3 = memberService.findById(1L);
+        assertEquals(Optional.empty(), byId3);
+
+
+
+        assertEquals("keesun@email.com",memberService.findById(1L).get().getEmail());
+        assertEquals("keesun@email.com",memberService.findById(2L).get().getEmail());
+
+        // 1이라는 값으로 find가 되면 런타임 익셉션을 던짐(스터빙)
+        // when(memberService.findById(1L)).thenThrow(new RuntimeException());
+
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+
     }
 
 }
