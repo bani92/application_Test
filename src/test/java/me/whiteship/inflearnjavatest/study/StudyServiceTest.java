55494a8589d019c2ccb561e77f67fed5a90d88b7
banseok <banseok9204@naver.com>
2024-06-28 22:01:47 +0900

    Mock 객체 스터빙 연습문제

--- src/test/java/me/whiteship/inflearnjavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnjavatest/study/StudyServiceTest.java
@@ -41,32 +41,22 @@ class StudyServiceTest {
         Member member = new Member();
         member.setId(1L);
         member.setEmail("keesun@email.com");
-        when(memberService.findById(any())).thenReturn(Optional.of(member)).thenThrow(new RuntimeException()).thenReturn(Optional.empty());
 
+        Study study = new Study(10, "테스트");
+
+        // memberService 객체에 findById 메소드를 1L 값으로 호출하면 member 객체를 리턴하도록 Stubbing
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
         Optional<Member> byId = memberService.findById(1L);
+
         assertEquals("keesun@email.com",byId.get().getEmail());
 
+        // studyRepository 객체에 save 메소드를 study 객체로 호출하면 study 객체 그대로 리턴하도록 Stubbing
+        when(studyRepository.save(study)).thenReturn(study);
 
-        assertThrows(RuntimeException.class, () -> {
-            memberService.findById(1L);
-        });
+        studyService.createNewStudy(1L, study);
 
-        Optional<Member> byId3 = memberService.findById(1L);
-        assertEquals(Optional.empty(), byId3);
-
-
-
-        assertEquals("keesun@email.com",memberService.findById(1L).get().getEmail());
-        assertEquals("keesun@email.com",memberService.findById(2L).get().getEmail());
-
-        // 1이라는 값으로 find가 되면 런타임 익셉션을 던짐(스터빙)
-        // when(memberService.findById(1L)).thenThrow(new RuntimeException());
-
-        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
-        assertThrows(IllegalArgumentException.class, () -> {
-            memberService.validate(1L);
-        });
-
+        assertNotNull(study.getOwnerId());
+        assertEquals(1, study.getOwnerId());
     }
 
 }
